b9d780982022b21b52ada8b96c26d0ac7ddeaec5
Charlie Sparks <sparksfullstack@gmail.com>
2020-03-25 11:56:32 -0400

    Updates

---
+++ precache-manifest.f750f9933ecda561a5eab4ffe1d07391.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54d994d04db94c61c42c24b05647637b",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "fe2f92f84cde43cfdf34",
+    "url": "/portfolio/static/css/2.257c7e99.chunk.css"
+  },
+  {
+    "revision": "5732bc50244cd24fe894",
+    "url": "/portfolio/static/css/main.8d854c14.chunk.css"
+  },
+  {
+    "revision": "fe2f92f84cde43cfdf34",
+    "url": "/portfolio/static/js/2.62fe2b24.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/portfolio/static/js/2.62fe2b24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5732bc50244cd24fe894",
+    "url": "/portfolio/static/js/main.a6ce038a.chunk.js"
+  },
+  {
+    "revision": "ceb68d410db72963d4f7",
+    "url": "/portfolio/static/js/runtime-main.3fbf267a.js"
+  },
+  {
+    "revision": "84a12224aeff9e4ec5e2cd2565dbb39c",
+    "url": "/portfolio/static/media/about-7.84a12224.jpg"
+  },
+  {
+    "revision": "20fc0c3535b80e122fdcdc791a12d1f6",
+    "url": "/portfolio/static/media/wedding suit circular.20fc0c35.png"
+  }
+]);
