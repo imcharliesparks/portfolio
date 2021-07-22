a30a3a72ed1fa21441133a3f6e673557e694b98d
Charlie Sparks <charlie.sparks@kroger.com>
2021-07-22 18:13:33 -0400

    Updated copyright tag

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,37 +1,41 @@
-import React from 'react';
-import {FaTwitter, FaGithub, FaLinkedinIn, FaEnvelope} from "react-icons/fa";
+import React from 'react'
+import { FaTwitter, FaGithub, FaLinkedinIn, FaEnvelope } from 'react-icons/fa'
 
 const SocialShare = [
-    {Social: <FaLinkedinIn /> , link: 'https://www.linkedin.com/in/sparksfullstack/'},
-    {Social: <FaGithub /> , link: 'https://github.com/SparksFullStack'},
-    {Social: <FaTwitter /> , link: 'https://twitter.com/SparksFullStack'},
-    {Social: <FaEnvelope /> , link: 'mailto:sparksfullstack@gmail.com'},
+  { Social: <FaLinkedinIn />, link: 'https://www.linkedin.com/in/sparksfullstack/' },
+  { Social: <FaGithub />, link: 'https://github.com/SparksFullStack' },
+  { Social: <FaTwitter />, link: 'https://twitter.com/SparksFullStack' },
+  { Social: <FaEnvelope />, link: 'mailto:sparksfullstack@gmail.com' }
 ]
 
 const Footer = () => {
-    return (
-        <div id='contact' className="footer-style-2 ptb--15 bg_image bg_image--1" data-black-overlay="6">
-            <div className="wrapper plr--50 plr_sm--20">
-                <div className="row align-items-center justify-content-between">
-                    <div className="col-12">
-                        <div className="inner text-center">
-                            <ul className="social-share rn-lg-size d-flex justify-content-center liststyle">
-                                {SocialShare.map((val , i) => (
-                                    <li key={i}><a target='_blank' rel='noopener noreferrer' href={`${val.link}`}>{val.Social}</a></li>
-                                ))}
-                            </ul>
-                        </div>
-                    </div>
-                    <div className="col-12">
-                        <div className="inner text-center mt--20">
-                            <div className="text">
-                                <p>Copyright Charlie Sparks© 2020</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <div id="contact" className="footer-style-2 ptb--15 bg_image bg_image--1" data-black-overlay="6">
+      <div className="wrapper plr--50 plr_sm--20">
+        <div className="row align-items-center justify-content-between">
+          <div className="col-12">
+            <div className="inner text-center">
+              <ul className="social-share rn-lg-size d-flex justify-content-center liststyle">
+                {SocialShare.map((val, i) => (
+                  <li key={i}>
+                    <a target="_blank" rel="noopener noreferrer" href={`${val.link}`}>
+                      {val.Social}
+                    </a>
+                  </li>
+                ))}
+              </ul>
             </div>
+          </div>
+          <div className="col-12">
+            <div className="inner text-center mt--20">
+              <div className="text">
+                <p>Copyright Charlie Sparks© 2021</p>
+              </div>
+            </div>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  )
 }
-export default Footer;
+export default Footer
